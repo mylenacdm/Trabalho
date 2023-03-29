e0fafeeb9be698d88def731f6a1bbe1038563ca4
Mylena Carvalho <111573062+mylenacdm@users.noreply.github.com>
2023-03-29 20:40:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 function calcular() {
-    const peso = parseFloat(document.getElementById("peso").value);
-    const altura = parseFloat(document.getElementById("altura").value);
-    const imc = peso / (altura * altura);
-    if (imc < 18.5) {
-        document.getElementById("resultado").textContent = "Seu IMC é: " + imc.toFixed(2) + (" Classificação: Magreza");
-    }
-    if (imc >= 18.5 && imc <= 24.9) {
-        document.getElementById("resultado").textContent = "Seu IMC é: " + imc.toFixed(2) + (" Classificação: Normal");
-    }
-    if (imc >= 25 && imc <= 29.9) {
-        document.getElementById("resultado").textContent = "Seu IMC é: " + imc.toFixed(2) + (" Classificação: Sobrepeso");
-    }
-    if (imc >= 30.0 && imc <= 39.9) {
-        document.getElementById("resultado").textContent = "Seu IMC é: " + imc.toFixed(2) + (" Classificação: Obesidade");
-    }
-    if (imc > 40) {
-        document.getElementById("resultado").textContent = "Seu IMC é: " + imc.toFixed(2) + (" Classificação: Obesidade grave");
-    }
-    
+
+    let capital = parseFloat(document.getElementById("capital").value);
+    let juros = parseFloat(document.getElementById("juros").value);
+    let periodo = parseInt(document.getElementById("periodo").value);
+  
+    let juroSimples = capital * (juros/100) * periodo;
+  
+    let composto = capital * Math.pow((1 + (juros/100)), periodo);
+    let jurosComposto = composto - capital;
+  
+    document.getElementById("simples").value = juroSimples.toFixed(2);
+    document.getElementById("composto").value = jurosComposto.toFixed(2);
 }
 
-let mudarcor1 = document.getElementById("evento")
 
-mudarcor1.addEventListener("mouseover", () => {
-    mudarcor1.style.background = "#45726e"
-})
 
-mudarcor1.addEventListener("click", () => {
-    mudarcor1.style.background = "white"
-})
-
-mudarcor1.addEventListener("mouseout", () => {
-    mudarcor1.style.background = "#50a199"
-})
+  
